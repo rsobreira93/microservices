df8e7e4e4a7fe38e1323d1637de6edf9ce969236
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-09 08:24:39 -0300

    add method create to product service

--- purchases/src/services/product.service.ts
+++ purchases/src/services/product.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
 import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateProductParams {
+  title: string;
+}
 
 @Injectable()
 export class ProductService {
@@ -7,5 +13,26 @@ export class ProductService {
 
   listAllProducts() {
     return this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: CreateProductParams) {
+    const slug = slugify(title, { lower: true });
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with the same slug already exists');
+    }
+
+    return await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
   }
 }
