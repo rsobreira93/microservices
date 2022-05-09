1fdc4df401ee80c9849f07663a5e520296aae98d
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-09 08:24:49 -0300

    add method create to product resolver

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductService } from '../../../services/product.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -12,5 +13,11 @@ export class ProductsResolver {
   // @UseGuards(AuthorizationGuard)
   products() {
     return this.productService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productService.createProduct(data);
   }
 }
