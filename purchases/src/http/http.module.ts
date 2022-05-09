afe13b6f189fefefc6cd74f87ad1467e427e56f3
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-09 07:55:58 -0300

    add product service to http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,7 +4,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from '../database/database.module';
 import path from 'node:path';
 import { ApolloDriver } from '@nestjs/apollo';
-import { TestResolver } from './auth/test.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { ProductService } from '../services/product.service';
 
 @Module({
   imports: [
@@ -15,6 +16,6 @@ import { TestResolver } from './auth/test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [ProductsResolver, ProductService],
 })
 export class HttpModule {}
