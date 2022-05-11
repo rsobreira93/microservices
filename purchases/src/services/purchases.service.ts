c45e71a6ff55858163e2a4d9b907fcff18356d12
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-11 08:46:16 -0300

    add method create to purchases

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface CreatePurchaseParams {
+  customerId: string;
+  productId: string;
+}
+
 @Injectable()
 export class PurchasesService {
   constructor(private prisma: PrismaService) {}
@@ -10,6 +15,25 @@ export class PurchasesService {
     return this.prisma.purchase.findMany({
       orderBy: {
         createAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ customerId, productId }: CreatePurchaseParams) {
+    const product = this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customerId,
+        productId,
       },
     });
   }
