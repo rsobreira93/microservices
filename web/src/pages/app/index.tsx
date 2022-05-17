4024077dee8a89a773ec4d0d31549391379005b5
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-17 07:48:06 -0300

    add new functio to get token on web app

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,4 +1,4 @@
-import {  useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import {  getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
 
 export default function Home(){
 
@@ -12,4 +12,12 @@ export default function Home(){
     )
 }
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+    getServerSideProps: async ({req, res}) =>{
+        console.log(getAccessToken(req, res));
+
+        return {
+            props:{}
+        }
+    }
+});
